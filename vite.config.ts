04ef7fbf5bedd9a4c4379b91158978631b6bf2d3
narendra101 <narendra11470@gmail.com>
2025-09-29 15:21:10 +0530

    :zap: | update in the config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: '/narendra101.github.io',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
