016200fda11c158f4d15346fae4278302fa84bc5
narendra101 <narendra11470@gmail.com>
2026-05-10 22:39:27 +0530

    :zap: | update in config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: "narendra101.github.io", 
+    // base: "narendra101.github.io", 
+    base: "/", 
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
